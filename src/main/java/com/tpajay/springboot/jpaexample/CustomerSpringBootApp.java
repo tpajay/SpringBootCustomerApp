ed6bd622f4d8dffbb1c0ac4bc4eb54454f18ad79
Jason Muse <jason.muse@gmail.com>
2017-05-03 16:58:31 -0400

    Updates

--- src/main/java/com/tpajay/springboot/jpaexample/CustomerSpringBootApp.java
+++ src/main/java/com/tpajay/springboot/jpaexample/CustomerSpringBootApp.java
@@ -25,36 +25,17 @@ public class CustomerSpringBootApp {
 		return (args) -> {
 			
 			
-			repository.save(new Customer("Jack", "Bauer"));
-			repository.save(new Customer("Chloe", "O'Brian"));
-			repository.save(new Customer("Kim", "Bauer"));
-			repository.save(new Customer("David", "Palmer"));
-			repository.save(new Customer("Michelle", "Dessler"));
+			repository.save(new Customer("John", "Smith"));
+			repository.save(new Customer("Bob", "Marley"));
+			repository.save(new Customer("Kurt", "Cobain"));
+			repository.save(new Customer("Sid", "Vicious"));
+			repository.save(new Customer("Fraiser", "Crane"));
+			repository.save(new Customer("Niles", "Crane"));
 
-			// fetch all customers
-			log.info("Customers found with findAll():");
-			log.info("-------------------------------");
+			// List all Customers in LOG
 			for (Customer customer : repository.findAll()) {
 				log.info(customer.toString());
 			}
-			log.info("");
-
-			// fetch an individual customer by ID
-			Customer customer = repository.findOne(1L);
-			log.info("Customer found with findOne(1L):");
-			log.info("--------------------------------");
-			log.info(customer.toString());
-			log.info("");
-
-			// fetch customers by last name
-			//log.info("Customer found with findByLastName('Bauer'):");
-			//log.info("--------------------------------------------");
-			//for (Customer bauer : repository.findByLastName("Bauer")) {
-			//	log.info(bauer.toString());
-			//}
-			//log.info("");
-			
-			
 		};
 	}
 
